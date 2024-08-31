ad774b501b3dd6f6ad21c86c2f7fd92fb51c1ce4
limtong <gustjd9610@gmail.com>
2024-08-31 15:35:53 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.13 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,6 +1,6 @@
 function solution(date1, date2) {
-  const dateStr1 = new Date(date1.join("-"));
-  const dateStr2 = new Date(date2.join("-"));
+  const dateStr1 = new Date(date1);
+  const dateStr2 = new Date(date2);
 
   return dateStr1 < dateStr2 ? 1 : 0;
 }
