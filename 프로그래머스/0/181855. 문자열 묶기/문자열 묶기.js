c12d00d0348a3aa9057e9632de6ad2795f1bec23
limtong <gustjd9610@gmail.com>
2024-09-03 09:06:43 +0900

    [level 0] Title: 문자열 묶기, Time: 11.53 ms, Memory: 50.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -0,0 +1,11 @@
+function solution(strArr) {
+  const numArr = strArr.map((v) => v.length);
+
+  const frequency = {};
+
+  for (let num of numArr) {
+    frequency[num] = (frequency[num] || 0) + 1;
+  }
+
+  return Math.max(...Object.keys(frequency).map((v) => frequency[v]));
+}
