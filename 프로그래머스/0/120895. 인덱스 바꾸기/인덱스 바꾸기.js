61c50bd48c7f11d6e394e60f89b4311d9509db14
limtong <gustjd9610@gmail.com>
2024-09-11 15:32:08 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,7 @@
+function solution(my_string, num1, num2) {
+  return [...my_string]
+    .map((v, i) =>
+      i === num1 ? my_string[num2] : i === num2 ? my_string[num1] : v
+    )
+    .join("");
+}
