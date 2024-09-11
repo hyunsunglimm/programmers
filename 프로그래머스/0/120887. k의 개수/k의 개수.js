800e6509206204b28350a0392d251d1ce4693159
limtong <gustjd9610@gmail.com>
2024-09-11 18:52:13 +0900

    [level 0] Title: k의 개수, Time: 41.86 ms, Memory: 49.9 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/0/120887. k의 개수/k의 개수.js
@@ -2,7 +2,6 @@ function solution(i, j, k) {
   return Array(j - i + 1)
     .fill()
     .map((_, index) => String(index + i))
-    .filter((v) => v.includes(String(k)))
     .join("")
     .split("")
     .filter((v) => v === String(k)).length;
