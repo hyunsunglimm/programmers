fdeb7cce277ade8b6fc9a519155a365109639973
limtong <gustjd9610@gmail.com>
2024-09-17 16:32:12 +0900

    [level 1] Title: 카드 뭉치, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,12 @@
+function solution(cards1, cards2, goal) {
+  for (const v of goal) {
+    if (v === cards1[0]) {
+      cards1.shift();
+    } else if (v === cards2[0]) {
+      cards2.shift();
+    } else {
+      return "No";
+    }
+  }
+  return "Yes";
+}
