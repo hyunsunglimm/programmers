7a1e2bd5931c0ef9e8bac5ff170a395cc49260b7
limtong <gustjd9610@gmail.com>
2024-10-21 17:12:01 +0900

    [level 1] Title: 과일 장수, Time: 273.92 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,17 +1,8 @@
 function solution(k, m, score) {
-  const bundle = Math.ceil(score.length / m);
-  const result = Array(bundle)
-    .fill()
-    .map(() => []);
   score.sort((a, b) => b - a);
-  score
-    .map((s, i) => ({ group: Math.floor(i / m), value: s }))
-    .forEach((v) => {
-      result[v.group].push(v.value);
-    });
-
-  return result
-    .filter((v) => v.length === m)
-    .map((v) => Math.min(...v) * m)
-    .reduce((a, b) => a + b, 0);
+  return (
+    score
+      .filter((_, i) => (i + 1) % m === 0)
+      .reduce((acc, cur) => acc + cur, 0) * m
+  );
 }
