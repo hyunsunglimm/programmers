705abbac9e33e84ca27401acc871736140482be8
limtong <gustjd9610@gmail.com>
2024-11-20 11:45:32 +0900

    [level 1] Title: 실패율, Time: 4.05 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,12 +1,16 @@
 function solution(N, stages) {
+  const stageCounts = Array(N + 2).fill(0);
+  stages.forEach((stage) => stageCounts[stage]++);
+
+  let totalPlayers = stages.length;
   const result = [];
 
   for (let i = 1; i <= N; i++) {
-    result.push([
-      i,
-      stages.filter((v) => v === i).length /
-        stages.filter((v) => v >= i).length,
-    ]);
+    const failed = stageCounts[i];
+    const failureRate = failed / totalPlayers || 0;
+    result.push([i, failureRate]);
+    totalPlayers -= failed;
   }
+
   return result.sort((a, b) => b[1] - a[1]).map((v) => v[0]);
 }
