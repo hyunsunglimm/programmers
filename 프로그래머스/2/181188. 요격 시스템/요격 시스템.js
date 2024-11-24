faca466e7327eea84261376d05bb3edab36751ed
limtong <gustjd9610@gmail.com>
2024-11-24 16:56:01 +0900

    [level 2] Title: 요격 시스템, Time: 683.49 ms, Memory: 168 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -0,0 +1,15 @@
+function solution(targets) {
+  targets.sort((a, b) => a[1] - b[1]);
+
+  let count = 0;
+  let lastIntercept = -Infinity;
+
+  for (const [start, end] of targets) {
+    if (start >= lastIntercept) {
+      count++;
+      lastIntercept = end;
+    }
+  }
+
+  return count;
+}
